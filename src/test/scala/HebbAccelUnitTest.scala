14b1967fc3028d03f835249b7bc91f0f1f8a98b8
vyeevani <vyeevani@berkeley.edu>
2020-05-03 22:50:50 -0700

    Competition is working as expected as well. In this test, there are two different inputs that are alternated between with a distinct hyperplane between the two high dimensional inputs. This leads to two very different weight features as expected

--- src/test/scala/HebbAccelUnitTest.scala
+++ src/test/scala/HebbAccelUnitTest.scala
@@ -18,15 +18,29 @@ import scala.io.Source
 // } 
 
 class HebbainAcceleratorPeekPokeTester[T<:FixedPoint](c: HebbianAccelerator[T]) extends DspTester(c) {
-    for (i <- 0 to 9) {
-        poke(c.io.in.bits(i), 1.0)
-    }
+    // for (i <- 0 to 9) {
+    //     poke(c.io.in.bits(i), 1.0)
+    // }
 
-    poke(c.io.layer_index, 0)
-    poke(c.io.weight_req_index, 0)
-    poke(c.io.weight_req_feature, 0)
+    
 
     for (i <- 0 to 20) {
+        if (i % 2 == 0) {
+            for (i <- 0 to 5) {
+                poke(c.io.in.bits(i), 1.0)
+            }
+            for (i <- 6 to 9) {
+                poke(c.io.in.bits(i), 0.0)
+            }
+        } else {
+            for (i <- 0 to 5) {
+                poke(c.io.in.bits(i), 0.0)
+            }
+            for (i <- 6 to 9) {
+                poke(c.io.in.bits(i), 1.0)
+            }
+        }
+
         poke(c.io.in.valid, 1) // notify that the input is valid
         poke(c.io.out.ready, 1) // notify that we are ready to recieve output
         step(1)
@@ -36,6 +50,16 @@ class HebbainAcceleratorPeekPokeTester[T<:FixedPoint](c: HebbianAccelerator[T]) 
         }
         step(1)
     }
-
-    peek(c.io.weight_req_response)
+    for (i <- 0 to 10) {
+        poke(c.io.layer_index, 0)
+        poke(c.io.weight_req_index, 0)
+        poke(c.io.weight_req_feature, i)
+        peek(c.io.weight_req_response)
+    }
+    for (i <- 0 to 10) {
+        poke(c.io.layer_index, 0)
+        poke(c.io.weight_req_index, 1)
+        poke(c.io.weight_req_feature, i)
+        peek(c.io.weight_req_response)
+    }
 }
